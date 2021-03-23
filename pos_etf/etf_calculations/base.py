687e6985d63e486b9fcc28f62dad6d43271fa46f
dan murphy <danielmurph8@gmail.com>
2021-03-22 22:59:43 -0400

    setup parent class for ETF weighting functions

---
+++ pos_etf/etf_calculations/base.py
@@ -0,0 +1,17 @@
+
+
+
+class EtfBase(object):
+    """
+    Base ETF class that contains useful functions used across various the
+    ETF-weighting strategies that inherit from it. Each uniue ETF-weighting 
+    strategy defined in this folder inherits from this class and utilizes its
+    functionality to calculate useful information for the ETF.
+
+    Attributes:
+        [TODO]
+    
+    Methods:
+        [TODO]
+    """
+    
