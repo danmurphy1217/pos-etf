3864c2641348fc20d1fbcbd82123e5cd2e37e43e
dan murphy <danielmurph8@gmail.com>
2021-03-23 18:49:47 -0400

    add functions to structure and format data for each coin

--- pos_etf/etf_calculations/base.py
+++ pos_etf/etf_calculations/base.py
@@ -1,6 +1,7 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
 import asyncio
 import aiohttp
+import json
 
 
 class BaseEtf(object):
@@ -70,18 +71,59 @@ class BaseEtf(object):
             self.coins_tuple), "length of market caps list != length of coins list"
 
         return list_of_coin_data
+
+    def extract(self, key_to_extract, object_to_extract_from: Dict[str, int or float or str]) -> int or float or str:
+        """
+        extract `key_to_extract` from `object_to_extract_from` and return it.
+
+        :param key_to_extract -> ``str``: the key to extract from the JSON object.
+        :param object_to_extract_from -> ``Dict[str, int or float or str]``: the object to extract data from.
+
+        :return -> int or float or str: the value associated with `key_to_extract` in the JSON object.
+        """
+        return object_to_extract_from[key_to_extract]
+
+    def structure(self, coin_tuples: List[Tuple[str, str, str]]) -> Dict[str, Dict[str, int or float]]:
+
+        etf_datapoints_dict = dict()
+
+        for coin_slug, json_data in coin_tuples:
+            filtered_json_data = json_data["data"]["statistics"]
+            etf_datapoints_dict[coin_slug] = self._build_json_dump('marketCap', 'circulatingSupply', data=filtered_json_data)
+
+        return etf_datapoints_dict
     
-    def pprint_coin_data(self, *keys_to_extract: str) -> None:
-        """
-        pretty print useful statistics for each coin.
+    def _build_json_dump(self, *args, **kwargs):
+        """build a JSON dump from the `kwargs` passed to the func"""
+        json_dump = dict()
+        for value_to_exact in args:
+            json_dump[value_to_exact] = self.extract(value_to_exact, kwargs.get("data"))
         
-        :keys_to_extract -> ``str``: the keys to attract from the statistics portion of the Coin Market Cap JSON Object.
-        :return -> ``None``:
-        """
-        print(keys_to_extract)
+        return json_dump
 
 
-Base = BaseEtf("https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail?slug=")
-# print(asyncio.run(Base.get_coins()))
-test_identifiers = ('marketCap', 'circulatingSupply')
-Base.pprint_coin_data(*test_identifiers)
+    # def pprint_coin_data(self, *keys_to_extract: str) -> None:
+    #     """
+    #     pretty print useful statistics for each coin.
+
+    #     :keys_to_extract -> ``str``: the keys to attract from the statistics portion of the Coin Market Cap JSON Object.
+    #     :return -> ``None``:
+    #     """
+
+
+Base = BaseEtf(
+    "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail?slug=")
+list_of_coin_response_objects = asyncio.run(Base.get_coins())
+
+
+print(Base.structure(list_of_coin_response_objects))
+# for coin_data in list_of_coin_response_objects:
+
+#     coin_name, response_obj = coin_data
+#     coin_statistics = response_obj["data"]["statistics"]
+#     print(Base.extract('marketCap', coin_statistics))
+#     print(Base.extract('circulatingSupply', coin_statistics))
+
+# print(Base.extract('marketCap', filtered_response_object))
+# test_identifiers = ('marketCap', 'circulatingSupply')
+# Base.pprint_coin_data(*test_identifiers)
