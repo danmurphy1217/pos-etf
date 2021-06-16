8fa4f7be1dcbb12fb5aaf28249d9718676b7cb0e
dan murphy <danielmurph8@gmail.com>
2021-06-16 15:36:40 -0400

    add utility func for cleaning acct names

--- pos_etf/cli/utils/__init__.py
+++ pos_etf/cli/utils/__init__.py
@@ -1,4 +1,24 @@
+import re
 from typing import List
+
+def clean_acct_names(user_dotfile: str) -> List[str]:
+    """
+    clean all account names and return them
+    
+    :param user_dotfile -> ``str``: path to users `.pos_etf` dotfile
+    """
+    all_acct_names = filter(
+        re.compile(
+            r"^\[[a-zA-Z0-9]").search, open(user_dotfile).readlines()
+    )
+
+    def formatted_acct_names(acct_names): return [
+        name.strip("[]\n") for name in acct_names
+    ]
+
+    list_of_formatted_acct_names = formatted_acct_names(all_acct_names)
+
+    return list_of_formatted_acct_names
 
 def extract_matching_pub_key(acct_name: str, dotfile_contents: List[str]):
     """
@@ -6,8 +26,6 @@ def extract_matching_pub_key(acct_name: str, dotfile_contents: List[str]):
 
     :param acct_name -> ``str``: the name of the account
     """
-    print(dotfile_contents)
-    print('My First Johnson' in dotfile_contents)
     index_of_acct_name = dotfile_contents.index(acct_name)
     index_of_pub_key = index_of_acct_name + 1
     
