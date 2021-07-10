f44edc8e10f81c3b989fd2ae15edeed5b0b2019a
Dan Murphy <danielmurph8@gmail.com>
2021-07-10 16:37:53 -0400

    add some refactoring for the transaction classes

--- pos_etf/cli/transaction/__init__.py
+++ pos_etf/cli/transaction/__init__.py
@@ -22,6 +22,67 @@ class Transaction:
         self.buy_or_sell_passphrase = buy_or_sell_passphrase
         self.algo_exchange_passphrase = algo_exchange_passphrase
         self.amount = amount
+    
+    def build_txn(self, txn_obj: PaymentTxn or AssetTransferTxn, **kwargs):
+        """
+        build a txn object based on the provided kwargs.
+
+        :param txn_obj -> ``PaymentTxn`` or ``AssetTransferTxn``: the txn object to pass the kwargs to.
+        :param kwargs -> ``dict``: the kwargs to pass to the txn object.
+        """
+        transfer_data = {
+            "sender": kwargs.get("sender"),
+            "receiver": kwargs.get("receiver"),
+            "amt": kwargs.get("amt")
+        }
+
+        if txn_obj == AssetTransferTxn:
+            transfer_data["index"] = kwargs.get("index")
+
+        data = add_network_params(self.transaction.client, transfer_data)
+        transaction = txn_obj(**data)
+        return transaction
+    
+    def send_txns(self, txns: PaymentTxn or AssetTransferTxn):
+        """
+        send txn to the algorand network
+        
+        :param txns -> ``PaymentTxn`` or ``AssetTransferTxn``: list of ``PaymentTxn`` or ``AssetTransferTxn`` transactions to.
+        """
+        def handle_algo_txn():
+            transaction_info = sign_and_send(
+                txn, self.algo_exchange_passphrase, self.client)
+            formatted_amount = balance_formatter(
+                self.amount, asset_id, self.client)
+            print("Transferred {} Algos from {} to {}".format(
+                self.amount, self.receiver_address, self.sender))
+
+            print("Transaction ID Confirmation: {}".format(
+                transaction_info.get("tx"))) 
+
+        def handle_etf_txn():
+            transaction_info = sign_and_send(
+                txn, self.buy_or_sell_passphrase, self.client)
+            formatted_amount = balance_formatter(
+                self.amount, asset_id, self.client)
+            print("Transferred {} from {} to {}".format(
+                formatted_amount, self.sender, self.receiver_address))
+            print("Transaction ID Confirmation: {}".format(
+                transaction_info.get("tx")))
+
+        for txn in txns:
+            if isinstance(txn, PaymentTxn):
+                if self.algo_exchange_passphrase:
+                    handle_algo_txn()       
+                else:
+                    write_to_file([txn], "transfer.txn")
+            
+            elif isinstance(txn, AssetTransferTxn):
+                if self.buy_or_sell_passphrase:
+                    handle_etf_txn()
+                else:
+                    write_to_file([txn], "transfer.txn")
+
 
     def do(self, *args):
         """
@@ -31,25 +92,24 @@ class Transaction:
         """
 
         assert all(txn_type in ('buy', 'sell', 'exchange') for txn_type in args)
-        
+        txns = []
+
         for txn_type in args:
-            print(txn_type)
-            # if txn_type not in ('buy', 'sell', 'exchange'):
-                # raise ArgumentError(None, "`txn_type` must be `buy`, `sell`, and/or `exchange`.")
-            if txn_type == 'buy':
-                txn_obj = Buy(self)
+            if txn_type == 'exchange':
+                txns.append(Exchange(self).build())
+            elif txn_type == 'buy':
+                txns.append(Buy(self).build())
             elif txn_type == 'sell':
-                txn_obj = Sell(self)
-            elif txn_type == 'exchange':
-                txn_obj = Exchange(self)
+                txns.append(Sell(self).build())
+        
+        
+        self.send_txns(txns)
 
-            txn_obj.transfer() # perform the txn
-
-class Buy():
+class Buy(Transaction):
     def __init__(self, transaction: Transaction):
         self.transaction = transaction
 
-    def transfer(self) -> None:
+    def build(self) -> None:
         """
         Creates an unsigned transfer transaction that is then sent off for signing
         and submission to the blockchain via the `sign_and_send` utility function.
@@ -69,21 +129,8 @@ class Buy():
             "index": asset_id,
         }
 
-        data = add_network_params(self.transaction.client, transfer_data)
-        transaction = AssetTransferTxn(**data)
-        print(data)
-
-        if self.transaction.buy_or_sell_passphrase:
-            transaction_info = sign_and_send(
-                transaction, self.transaction.buy_or_sell_passphrase, self.transaction.client)
-            formatted_amount = balance_formatter(
-                self.transaction.amount, asset_id, self.transaction.client)
-            print("Transferred {} from {} to {}".format(
-                formatted_amount, self.transaction.sender, self.transaction.receiver_address))
-            print("Transaction ID Confirmation: {}".format(
-                transaction_info.get("tx")))
-        else:
-            write_to_file([transaction], "transfer.txn")
+        transaction = self.build_txn(AssetTransferTxn, **transfer_data)
+        return transaction
 
 
 class Sell(Buy):
@@ -97,7 +144,7 @@ class Exchange(Sell):
         """
         super().__init__(transaction)
     
-    def transfer(self):
+    def build(self):
         """
         exchange algos for the token (from buyer -> seller if Sell(), seller -> buyer if Buy())
         """
@@ -111,19 +158,8 @@ class Exchange(Sell):
             "receiver": self.transaction.sender, # the person sending the token to another receives algos in return
             "amt": formatted_algo_amt,
         }
+        
+        transaction = self.build_txn(PaymentTxn, **payment_data)
+        print(transaction)
 
-        data = add_network_params(self.transaction.client, payment_data)
-        transaction = PaymentTxn(**data)
-
-        if self.transaction.algo_exchange_passphrase:
-            transaction_info = sign_and_send(
-                transaction, self.transaction.algo_exchange_passphrase, self.transaction.client)
-            formatted_amount = balance_formatter(
-                self.transaction.amount, asset_id, self.transaction.client)
-            print("Transferred {} Algos from {} to {}".format(
-                self.transaction.amount, self.transaction.receiver_address, self.transaction.sender))
-            print(transaction_info)
-            print("Transaction ID Confirmation: {}".format(
-                transaction_info.get("tx")))
-        else:
-            write_to_file([transaction], "transfer.txn")
+        return transaction
